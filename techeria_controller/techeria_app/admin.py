5e6500d1202521b7412455621509d8462e7fd1a3
farahh001 <fsultan001@citymail.cuny.edu>
2021-11-22 20:34:37 -0500

    with modified models

--- techeria_controller/techeria_app/admin.py
+++ techeria_controller/techeria_app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from techeria_app.models import Products,BuyerModel,Laptops
+from techeria_app.models import Products,BuyerModel
 
 admin.site.register(Products)
-admin.site.register(Laptops)
 admin.site.register(BuyerModel)
-# admin.site.register(Smartphone)
